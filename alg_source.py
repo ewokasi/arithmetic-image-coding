4a500a204210389640bdf81bc14b9ab8aa41d48f
ewokasi <kostyakov0203@mail.ru>
2023-06-28 00:59:52 +0300

    поддержка длинного текста
    270 байт json -> 65 байт

--- alg_source.py
+++ alg_source.py
@@ -1,10 +1,13 @@
 import json
 
 def get_probs(string):
+    #print(string)
+    string=str(string)
     if string=="" or string ==None:
         print("The input string is None or empty")
         return 0
-    string+="!"
+    
+    string= string+"!"
     alphabet = string
     length = len(string)
     probs = {}
@@ -29,7 +32,7 @@ def get_location(probs, entering = 0, finish =1):
 
 
 def select(start, end):
-    print(start, end)
+    #print(start, end)
     start = str(start)
     end = str(end)
     res = ""
@@ -38,12 +41,12 @@ def select(start, end):
         if start[i]==end[i]:
             res+=start[i]
         else:
-            print("sss",start[i])
+            #print("sss",start[i])
             if int(end[i])-int (start[i])>=1:
                 res+=str(int(start[i])+1)
                 break
-    print(res)
-    print(start, end)
+    #print(res)
+    #print(start, end)
     return res
 
 
@@ -56,18 +59,18 @@ def compression(string):
     right = 1
 
     for alpha in string:
-        print("iteration", alpha)
+        #print("iteration", alpha)
         left = location[alpha][0]
         right = location[alpha][1]
         location = get_location(probs, left, right)
-        print("calcs" ,location)
+        #print("calcs" ,location)
       
     compressed = select(left, right)
-    print("select" ,compressed)
+    #print("select" ,compressed)
     archive ={}
-    archive['probs']=probs
-    archive['compressed']=compressed
-    save(archive, "test")
+    archive['p']=probs
+    archive['c']=compressed
+    return archive
 
 def save(archive, name):
     with open(f'{name}.json', 'w') as outfile:
@@ -85,43 +88,81 @@ def o_format(data):
 def location_to_alpha(location, data):
     length = len(location)
     #print("location", list(location.values()))
-  
+    
     for i in range(length):
      
         left  = list(location.values())[i][0]
         right = list(location.values())[i][1]
-        print("iteration ",i, "left", left, "right", right, "data", data)
+        #print("iteration ",i, "left", left, "right", right, "data", data, "delta",right-left) 
+        
         if left<=data and right >=data:
-            print("________________returned_________ ", list(location)[i])
+    
             return list(location)[i]
         
             
+def separator(string):
+    dataset = {}
+    sector=""
+    iter=0
+    for i in range(len(string)):
+        sector+=str(string[i])
+        if i%13==0 and i!=0:
+            dataset[iter]=sector
+            iter+=1
+            sector=""
+        if i == len(string)-1:
+            dataset[iter]=sector
+            sector=""
+    return dataset        
 
     
-def decompression(name):
-    archive = load(f"{name}")
-    probs = archive["probs"]
-    data = o_format(archive["compressed"])
+def decompression(archive):
+   
+    probs = archive["p"]
+    data = o_format(archive["c"])
    
     left = 0
     right = 1
-    location = get_location(probs, left, right)
+    
     decompressed = ""
-    alpha = location_to_alpha(location, data)
+    alpha = ""
     while alpha!="!":
-        decompressed+= alpha
-        left = location[alpha][0]
-        right = location[alpha][1]
         location = get_location(probs, left, right)
         
         alpha = location_to_alpha(location, data)
+        if alpha=="!":
+            break
+        decompressed+= alpha
+        left = location[alpha][0]
+        right = location[alpha][1]
+        
     return decompressed
     
-if __name__=="__main__":
-    test = "hello world everyone"
-    print(get_probs(test))
-    compression(test)
-    print(decompression("test.json"))
+
+def long_compression(string):
+
+    dataset=separator(string)
+    compressed={}
+    hashed={}
+    for i in dataset:
+        compressed[i]=compression(dataset[i])
+        hashed[i]=compressed[i]['c']
+    save(compressed, "test")  
+    save(hashed, "pure_test")
+
+def long_decompression(name):
+    archive = load(f"{name}")
+    
+    res=""
+    for i in range(len(archive)):
+        res+=decompression(archive[f'{i}'])
+    return res
+
+if __name__=="__main__":#13 уникальных символов
+    test = "hello world and everyone who wathes"
+ 
+    long_compression(test)
+    print(long_decompression("test.json"))
     
   
    
